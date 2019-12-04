1024e6a9899689aaf2fb33a6ac6c994763be54a7
Bennett Rennier <bennett@brennier.com>
2019-12-04 15:06:55 -0500

    Improve error-handling

--- extract-dialogue.sh
+++ extract-dialogue.sh
@@ -15,12 +15,15 @@ Only the -i option is required. If not specified, the default behavior is to use
 the first audio track and the first subtitle track. The default output name is
 "output.mp3". Similar to ffmpeg, the extension of the output name determines the
 format of the output.\n' "$(basename $0)"
+    exit 0
 }
 
-if [ -z "$1" ]; then
-    usage
-    exit
-fi
+error () {
+    echo "Error: $1" >&2
+    exit 1
+}
+
+[ -z "$1" ] && usage
 
 while [ -n "$1" ]; do
     case "$1" in
@@ -28,8 +31,8 @@ while [ -n "$1" ]; do
         "-a") shift; audio="$1"  ;;
         "-s") shift; subs="$1"   ;;
         "-o") shift; output="$1" ;;
-        "-h") usage ; exit       ;;
-        *) echo "There was an error parsing arguments. Make sure to use the -i option." >&2 ; exit 1 ;;
+        "-h") usage              ;;
+        *) error "There was an error parsing arguments. Make sure to use the -i option." ;;
     esac
     shift
 done
@@ -37,19 +40,18 @@ done
 audio_id=$(ffprobe "$file" 2>&1 | grep "Audio" | sed -n "${audio:-1}p" | grep -o '[0-9]:[0-9]')
 subs_id=$(ffprobe "$file" 2>&1 | grep "Subtitle" | sed -n "${subs:-1}p" | grep -o '[0-9]:[0-9]')
 
-if [ -z $subs_id ]; then
-    echo "Error: No text-based subtitles found." >&2
-    exit 1
-fi
+[ -z "$subs_id" ] && error "No text-based subtitles found."
 
-ffmpeg -i "$file" -map $subs_id "$temp/subs.ass" 2> /dev/null
+ffmpeg -loglevel fatal -i "$file" -map $subs_id "$temp/subs.ass"
 timestamps=$(grep "^Dialogue:.*Default" "$temp/subs.ass" | cut -f "2,3" -d "," | tr '\n' ' ')
+
+[ -z "$timestamps" ] && error "Subtitles file was found, but parsing failed."
 
 num=1
 for timestamp in $timestamps; do
     begin=$(echo "$timestamp" | cut -f1 -d,)
     end=$(  echo "$timestamp" | cut -f2 -d,)
-    ffmpeg -ss "$begin" -to "$end" -i "$file" -map $audio_id "$temp/$num.mp3" 2> /dev/null
+    ffmpeg -loglevel fatal -ss "$begin" -to "$end" -i "$file" -map $audio_id "$temp/$num.mp3"
     if ffprobe -i "$temp/$num.mp3" 2> /dev/null; then
         echo "$num.mp3 : $begin -> $end"
         echo "file '$temp/$num.mp3'" >> "$temp/list.txt"
@@ -59,4 +61,4 @@ for timestamp in $timestamps; do
     fi
 done
 
-ffmpeg -safe 0 -f concat -i "$temp/list.txt" "${output:-output.mp3}"
+ffmpeg -loglevel fatal -safe 0 -f concat -i "$temp/list.txt" "${output:-output.mp3}"
