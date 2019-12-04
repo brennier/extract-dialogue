c440d1fd492f78bf48f5e1c715c47e27bada20b9
brennier <bennett@brennier.com>
2019-12-03 20:56:39 -0500

    Check that each mp3 is valid

--- extract-dialogue.sh
+++ extract-dialogue.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 temp=$(mktemp --directory)
-trap 'rm -rf $temp' EXIT
+# trap 'rm -rf $temp' EXIT
 
 ffmpeg -i "$1" -map 0:s:0 "$temp/subs.ass" 2> /dev/null
 timestamps=$(grep "^Dialogue:.*Default" "$temp/subs.ass" | cut -f "2,3" -d "," | tr '\n' ' ')
@@ -10,9 +10,13 @@ for timestamp in $timestamps; do
     begin=$(echo "$timestamp" | cut -f1 -d,)
     end=$(  echo "$timestamp" | cut -f2 -d,)
     ffmpeg -ss "$begin" -to "$end" -i "$1" "$temp/$num.mp3" 2> /dev/null
-    echo "$num.mp3 : $begin -> $end"
-    echo "file '$temp/$num.mp3'" >> "$temp/list.txt"
-    num="$(( $num + 1 ))"
+    if ffprobe -i "$temp/$num.mp3" 2> /dev/null; then
+        echo "$num.mp3 : $begin -> $end"
+        echo "file '$temp/$num.mp3'" >> "$temp/list.txt"
+        num="$(( $num + 1 ))"
+    else
+        rm "$temp/$num.mp3"
+    fi
 done
 
 ffmpeg -y -safe 0 -f concat -i $temp/list.txt -c copy output.mp3
