dd1c1430811e2da00ddecf57463e98a3553d8aa8
brennier <bennett@brennier.com>
2019-12-03 20:46:19 -0500

    Clean-up logic

--- extract-dialogue.sh
+++ extract-dialogue.sh
@@ -3,16 +3,16 @@ temp=$(mktemp --directory)
 trap 'rm -rf $temp' EXIT
 
 ffmpeg -i "$1" -map 0:s:0 "$temp/subs.ass" 2> /dev/null
-timestamps=$(grep "^Dialogue:" "$temp/subs.ass" | cut -f "2,3" -d "," | grep "Default" | tr '\n' ' ')
+timestamps=$(grep "^Dialogue:.*Default" "$temp/subs.ass" | cut -f "2,3" -d "," | tr '\n' ' ')
 
 num=1
 for timestamp in $timestamps; do
     begin=$(echo "$timestamp" | cut -f1 -d,)
     end=$(  echo "$timestamp" | cut -f2 -d,)
-    ffmpeg -ss "$begin" -to "$end" -i "$1" -q:a 0 -map a "$temp/$num.mp3" 2> /dev/null
-    echo "$num : $begin $end"
+    ffmpeg -ss "$begin" -to "$end" -i "$1" "$temp/$num.mp3" 2> /dev/null
+    echo "$num.mp3 : $begin -> $end"
+    echo "file '$temp/$num.mp3'" >> "$temp/list.txt"
     num="$(( $num + 1 ))"
 done
 
-find $temp -name '*.mp3' | sort -V | sed "s/\(.*\)/file '\1'/" > $temp/list.txt
 ffmpeg -y -safe 0 -f concat -i $temp/list.txt -c copy output.mp3
