1e1d2d7fcbe2b752efa284287388e708f64b3845
brennier <bennett@brennier.com>
2019-12-06 20:57:12 -0500

    Convert subtitles to .ass instead of breaking into cases

--- extract-dialogue.sh
+++ extract-dialogue.sh
@@ -23,19 +23,27 @@ error() {
     exit 1
 }
 
-[ -z "$1" ] && usage
-
-dialogues () {
+extract_timestamps() {
     # Parses the subtitles file to the format BEGIN,END
     # BEGIN and END are in the format HH:MM:SS.sss
     # H: hour, M: minute, S: second, s: milisecond
-    extension=$(echo "$1" | sed 's/.*\.\(.*\)/\1/')
-    if [ "$extension" = 'ass' ]; then
-        grep "^Dialogue:.*" "$1" | cut -f "2,3" -d "," | uniq | tr '\n' ' '
-    elif [ "$extension" = 'srt' ]; then
-        grep [0-9]*:[0-9]*:[0-9]*,[0-9]* "$1" | uniq | sed 's/,/./g;s/ --> /,/' | tr '\n' ' '
+    subs="$1"
+    if [ -f "$subs" ]; then
+        ffmpeg -loglevel fatal -i "$subs" "$temp/subs.ass"
+    else
+        subs_id=$(ffprobe "$file" 2>&1 | grep "Stream .*Subtitle" | sed -n "${subs:-1}p" | grep -o '[0-9]:[0-9]')
+        [ -z "$subs_id" ] && error "No text-based subtitles found in '$file'."
+        ffmpeg -loglevel fatal -i "$file" -map $subs_id "$temp/subs.ass"
     fi
+    timestamps=$(grep "^Dialogue:.*\(Default\|Main\)" "$temp/subs.ass" | cut -f "2,3" -d "," | tr '\n' ' ')
+
+    [ ! -f "$temp/subs.ass" ] && error "No subtitles file found."
+    [ -z "$timestamps" ] && error "Subtitles file was found, but parsing failed."
+
+    echo "$timestamps"
 }
+
+[ -z "$1" ] && usage
 
 while [ -n "$1" ]; do
     case "$1" in
@@ -50,27 +58,10 @@ while [ -n "$1" ]; do
 done
 
 audio_id=$(ffprobe "$file" 2>&1 | grep "Stream .*Audio" | sed -n "${audio:-1}p" | grep -o '[0-9]:[0-9]')
-
-# Look for an existing subtitles file, and, if absent, generate one from the video file
-if [ ! -f "$subs" ]; then
-    subs_id=$(ffprobe "$file" 2>&1 | grep "Stream .*Subtitle" | sed -n "${subs:-1}p" | grep -o '[0-9]:[0-9]')
-    [ -z "$subs_id" ] && error "No text-based subtitles found."
-    subs_file="$temp/subs.ass"
-    ffmpeg -loglevel fatal -i "$file" -map $subs_id "$subs_file"
-else
-    subs_file="$subs"
-fi
-
-[ ! -f "$subs_file" ] && error "No subtitles file found."
-
-timestamps=$(dialogues "$subs_file")
-
-[ -z "$timestamps" ] && error "Subtitles file was found, but parsing failed."
-
-num=1
-# Use the same extension as the output file for the intermediate files
 ext=$(echo "${output:=output.mp3}" | sed 's/.*\.\(.*\)/\1/')
-for timestamp in $timestamps; do
+num=1
+
+for timestamp in $(extract_timestamps "$subs"); do
     begin=$(echo "$timestamp" | cut -f1 -d,)
     end=$(  echo "$timestamp" | cut -f2 -d,)
     ffmpeg -y -loglevel fatal -ss "$begin" -to "$end" -i "$file" -map $audio_id "$temp/$num.$ext"
