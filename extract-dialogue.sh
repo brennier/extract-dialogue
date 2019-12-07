8cf5c1e3f37e280f83956dfe72fa51b8bd081545
brennier <bennett@brennier.com>
2019-12-06 22:08:51 -0500

    Compare timestamps lexicographically as strings

--- extract-dialogue.sh
+++ extract-dialogue.sh
@@ -27,6 +27,7 @@ extract_timestamps() {
     # Parses the subtitles file to the format BEGIN,END
     # BEGIN and END are in the format HH:MM:SS.sss
     # H: hour, M: minute, S: second, s: milisecond
+    # Output is sorted by BEGIN time
     subs="$1"
     if [ -f "$subs" ]; then
         ffmpeg -loglevel fatal -i "$subs" "$temp/subs.ass"
@@ -35,7 +36,7 @@ extract_timestamps() {
         [ -z "$subs_id" ] && error "No text-based subtitles found in '$file'."
         ffmpeg -loglevel fatal -i "$file" -map $subs_id "$temp/subs.ass"
     fi
-    timestamps=$(grep "^Dialogue:.*\(Default\|Main\)" "$temp/subs.ass" | cut -f "2,3" -d "," | tr '\n' ' ')
+    timestamps=$(grep "^Dialogue:.*\(Default\|Main\)" "$temp/subs.ass" | cut -f "2,3" -d "," | sort | tr '\n' ' ')
 
     [ ! -f "$temp/subs.ass" ] && error "No subtitles file found."
     [ -z "$timestamps" ] && error "Subtitles file was found, but parsing failed."
@@ -48,44 +49,21 @@ merge_timestamps() {
     # overlapping intervals
     cur_begin=""
     cur_end=""
-    for timestamp in $(cat - | tr ' ' '\n' | sort | tr '\n' ' '); do
+    for timestamp in $(cat /dev/stdin); do
         begin=$(echo "$timestamp" | cut -f1 -d,)
         end=$(  echo "$timestamp" | cut -f2 -d,)
         [ -z "$cur_begin" ] && cur_begin="$begin"
         [ -z "$cur_end" ] && cur_end="$end"
 
-        if earlier_than "$cur_end" "$begin"; then
+        if expr "$cur_end" \< "$begin"; then
             echo "$cur_begin,$cur_end "
             cur_begin=$begin
             cur_end=$end
-        elif earlier_than "$cur_end" "$end"; then
+        elif expr "$cur_end" \< "$end"; then
             cur_end="$end"
         fi
     done
     echo "$cur_begin,$cur_end"
-}
-
-earlier_than() {
-    # Takes two timestamps in the format of HH:MM:SS.ss and
-    # determines whether the first occurs earlier than the second
-    hour1=$(echo "$1" | cut -f1 -d:)
-    hour2=$(echo "$2" | cut -f1 -d:)
-    min1=$(echo "$1" | cut -f2 -d:)
-    min2=$(echo "$2" | cut -f2 -d:)
-    sec1=$(echo "$1" | cut -f3 -d: | cut -f1 -d.)
-    sec2=$(echo "$2" | cut -f3 -d: | cut -f1 -d.)
-    msec1=$(echo "$1" | cut -f2 -d.)
-    msec2=$(echo "$2" | cut -f2 -d.)
-
-    [ $hour1 -lt $hour2 ] && return 0
-    [ $hour1 -gt $hour2 ] && return 1
-    [ $min1  -lt $min2  ] && return 0
-    [ $min1  -gt $min2  ] && return 1
-    [ $sec1  -lt $sec2  ] && return 0
-    [ $sec1  -gt $sec2  ] && return 1
-    [ $msec1 -lt $msec2 ] && return 0
-    [ $msec1 -gt $msec2 ] && return 1
-    return 1 # If they're the same, return false
 }
 
 [ -z "$1" ] && usage
