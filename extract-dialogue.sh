116580bac090c864aec74c8fbf65f5813b0f5cbe
Bennett Rennier <bennett@brennier.com>
2019-12-08 16:13:46 -0500

    Remove quotations from variable assignments

--- extract-dialogue.sh
+++ extract-dialogue.sh
@@ -24,7 +24,7 @@ extract_timestamps() {
     # BEGIN and END are in the format HH:MM:SS.sss
     # H: hour, M: minute, S: second, s: milisecond
     # Output is sorted by BEGIN time
-    subs="$1"
+    subs=$1
     if [ -f "$subs" ]; then
         ffmpeg -loglevel fatal -i "$subs" "$temp/subs.ass"
     else
@@ -57,15 +57,15 @@ merge_timestamps() {
     for timestamp in $(cat /dev/stdin); do
         begin=$(echo "$timestamp" | cut -f1 -d,)
         end=$(  echo "$timestamp" | cut -f2 -d,)
-        [ -z "$cur_begin" ] && cur_begin="$begin"
-        [ -z "$cur_end" ]   && cur_end="$end"
+        [ -z "$cur_begin" ] && cur_begin=$begin
+        [ -z "$cur_end" ]   && cur_end=$end
 
         if expr "$cur_end" "<" "$begin" > /dev/null; then
             echo "$cur_begin,$cur_end"
-            cur_begin="$begin"
-            cur_end="$end"
+            cur_begin=$begin
+            cur_end=$end
         elif expr "$cur_end" "<" "$end" > /dev/null; then
-            cur_end="$end"
+            cur_end=$end
         fi
     done
     echo "$cur_begin,$cur_end"
@@ -75,18 +75,18 @@ pad_timestamps() {
     # Takes a list of timestamp intervals from standard input and a
     # number as its first argument. Pads the intervals by that number of
     # milliseconds. Padding must be less than 1 second.
-    padding="$(printf "%03d" "$1")"
+    padding=$(printf "%03d" "$1")
     [ "$padding" -gt 1000 ] && error "Padding is too large!"
     for timestamp in $(cat /dev/stdin); do
         begin=$(echo "$timestamp" | cut -f1 -d,)
         end=$(  echo "$timestamp" | cut -f2 -d,)
         # Use date to shift timestamps
         if expr "$begin" ">" "00:00:00.$padding" > /dev/null; then
-            new_begin="$(date +"%T.%2N" -d "$begin - 0.$padding seconds")"
+            new_begin=$(date +"%T.%2N" -d "$begin - 0.$padding seconds")
         else
             new_begin="00:00:00.00"
         fi
-        new_end="$(date +"%T.%2N" -d "$end + 0.$padding seconds")"
+        new_end=$(date +"%T.%2N" -d "$end + 0.$padding seconds")
 
         echo "$new_begin,$new_end"
     done
@@ -96,13 +96,13 @@ pad_timestamps() {
 
 while [ -n "$1" ]; do
     case "$1" in
-        "-i") shift; file="$1"    ;;
-        "-a") shift; audio="$1"   ;;
-        "-s") shift; subs="$1"    ;;
-        "-o") shift; output="$1"  ;;
-        "-p") shift; padding="$1" ;;
-        "-k") keep=1              ;;
-        "-h") usage               ;;
+        "-i") shift; file=$1    ;;
+        "-a") shift; audio=$1   ;;
+        "-s") shift; subs=$1    ;;
+        "-o") shift; output=$1  ;;
+        "-p") shift; padding=$1 ;;
+        "-k") keep=1            ;;
+        "-h") usage             ;;
         *) error "There was an error parsing arguments. Make sure to use the -i option." ;;
     esac
     shift
@@ -111,7 +111,7 @@ done
 temp=$(mktemp -d)
 [ -z "$keep" ] && trap 'rm -rf $temp' EXIT
 
-timestamps="$(extract_timestamps "$subs" | pad_timestamps "${padding:-100}" | merge_timestamps)"
+timestamps=$(extract_timestamps "$subs" | pad_timestamps "${padding:-100}" | merge_timestamps)
 audio_id=$(ffprobe "$file" 2>&1 | grep "Stream .*Audio" | sed -n "${audio:-1}p" | grep -o '[0-9]:[0-9]')
 ext=$(echo "${output:=output.mp3}" | sed 's/.*\.//')
 num="1"
@@ -124,7 +124,7 @@ for timestamp in $timestamps; do
     if ffprobe -i "$temp/$num.$ext" 2> /dev/null; then
         echo "$num.$ext : $begin -> $end"
         echo "file '$temp/$num.$ext'" >> "$temp/list.txt"
-        num="$(( $num + 1 ))"
+        num=$(( $num + 1 ))
     fi
 done
 
