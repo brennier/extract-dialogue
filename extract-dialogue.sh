773cd77347ffb20a72caa4055db60ac834a47bf8
Bennett Rennier <bennett@brennier.com>
2019-12-13 17:47:36 -0500

    Convert to milliseconds and use ffmpeg filter complexes

--- extract-dialogue.sh
+++ extract-dialogue.sh
@@ -6,7 +6,7 @@ Options:
     -a    Specify the audio track number to use
     -s    Either specify the subtitle track number to use or specify an external subtitle file
     -o    Specify the output filename
-    -p    Specify padding (in milliseconds) around subtitle timestamps; must be less than 1000
+    -p    Specify padding (in milliseconds) around subtitle timestamps
     -k    Keep intermediate files stored under /tmp; useful for debugging purposes
     -h    Display this usage message
 
@@ -20,11 +20,11 @@ error() {
 }
 
 extract_timestamps() {
-    # Parses the subtitles file to the format BEGIN,END
-    # BEGIN and END are in the format HH:MM:SS.sss
-    # H: hour, M: minute, S: second, s: milisecond
+    # Parses the subtitles file to the format BEGIN:END
+    # BEGIN and END are in milliseconds.
     # Output is sorted by BEGIN time
     subs=$1
+    padding=$2
     if [ -f "$subs" ]; then
         ffmpeg -loglevel fatal -i "$subs" "$temp/subs.ass"
     else
@@ -38,53 +38,37 @@ extract_timestamps() {
     # Extract the timestamps of dialogue without extra styling.
     # This will exclude signs and most OPs/EDs.
     timestamps=$(grep "^Dialogue:" "$temp/subs.ass" | grep -v ",{" \
-                 | cut -f "2,3" -d ",")
+                 | cut -f "2,3" -d "," | sort)
 
     [ -z "$timestamps" ] && error "Subtitles file was found, but parsing failed."
-    echo "$timestamps"
+
+    # Convert timestamps to milliseconds and pad the timestamps
+    echo "$timestamps" | awk -F ':|,' -v p=$padding '{ printf "%d:%d\n", \
+        ( $1 * 3600000 ) + ( $2 * 60000 ) + ($3 * 1000) - p, \
+        ( $4 * 3600000 ) + ( $5 * 60000 ) + ($6 * 1000) + p }'
 }
 
 merge_timestamps() {
     # Takes a list of timestamp intervals from standard input and merges
     # overlapping intervals
-    cur_begin=""
-    cur_end=""
+    cur_begin=
+    cur_end=
+
     for timestamp in $(cat /dev/stdin); do
-        begin=$(echo "$timestamp" | cut -f1 -d,)
-        end=$(  echo "$timestamp" | cut -f2 -d,)
+        begin=$(echo "$timestamp" | cut -f1 -d:)
+        end=$(  echo "$timestamp" | cut -f2 -d:)
         [ -z "$cur_begin" ] && cur_begin=$begin
         [ -z "$cur_end" ]   && cur_end=$end
 
-        if expr "$cur_end" "<" "$begin" > /dev/null; then
-            echo "$cur_begin,$cur_end"
+        if [ "$cur_end" -lt "$begin" ]; then
+            echo "$cur_begin:$cur_end"
             cur_begin=$begin
             cur_end=$end
-        elif expr "$cur_end" "<" "$end" > /dev/null; then
+        elif [ "$cur_end" -lt "$end" ]; then
             cur_end=$end
         fi
     done
-    echo "$cur_begin,$cur_end"
-}
-
-pad_timestamps() {
-    # Takes a list of timestamp intervals from standard input and a
-    # number as its first argument. Pads the intervals by that number of
-    # milliseconds. Padding must be less than 1 second.
-    padding=$(printf "%03d" "$1")
-    [ "$padding" -gt 1000 ] && error "Padding is too large!"
-    for timestamp in $(cat /dev/stdin); do
-        begin=$(echo "$timestamp" | cut -f1 -d,)
-        end=$(  echo "$timestamp" | cut -f2 -d,)
-        # Use date to shift timestamps
-        if expr "$begin" ">" "00:00:00.$padding" > /dev/null; then
-            new_begin=$(date +"%T.%2N" -d "$begin - 0.$padding seconds")
-        else
-            new_begin="00:00:00.00"
-        fi
-        new_end=$(date +"%T.%2N" -d "$end + 0.$padding seconds")
-
-        echo "$new_begin,$new_end"
-    done
+    echo "$cur_begin:$cur_end"
 }
 
 [ -z "$1" ] && usage
@@ -106,29 +90,26 @@ done
 temp=$(mktemp -d)
 [ -z "$keep" ] && trap 'rm -rf $temp' EXIT
 
-timestamps=$(extract_timestamps "$subs" | pad_timestamps "${padding:-100}" \
-             | sort | merge_timestamps)
+timestamps=$(extract_timestamps "$subs" "${padding:-100}" | merge_timestamps \
+    | awk -F: '{ printf "%.3f:%.3f\n", ( $1 / 1000 ), ( $2 / 1000 ) }')
 audio_id=$(ffprobe "$file" 2>&1 | grep "Stream .*Audio" \
            | sed -n "${audio:-1}p" | grep -o '[0-9]:[0-9]')
 ext=$(echo "${output:=output.mp3}" | sed 's/.*\.//')
-num="1"
 
-echo "Extracting audio clips based on subtitle timestamps..."
+# Setup the filter complex command for trimming and then concatenating
+num=1
+trim=
+concat=
 for timestamp in $timestamps; do
-    begin=$(echo "$timestamp" | cut -f1 -d,)
-    end=$(  echo "$timestamp" | cut -f2 -d,)
-    ffmpeg -y -loglevel fatal -i "$file" -map $audio_id \
-           -ss "$begin" -to "$end" "$temp/$num.$ext"
-    if ffprobe -i "$temp/$num.$ext" 2> /dev/null; then
-        echo "$num.$ext : $begin -> $end"
-        echo "file '$temp/$num.$ext'" >> "$temp/list.txt"
-        num=$(( $num + 1 ))
-    fi
+    trim="$trim[$audio_id]atrim=$timestamp[a$num];"
+    concat="${concat}[a$num]"
+    num=$(( $num + 1 ))
 done
+num=$(( $num - 1 ))
+concat="${concat}concat=n=$num:v=0:a=1[out]"
 
 base=$(basename "$file" | sed 's/\.[^.]*$//')
 echo "Concatenating audio files..."
-ffmpeg -loglevel fatal -safe 0 -f concat \
-       -i "$temp/list.txt" "${output:-$base.mp3}"
+ffmpeg -i "$file" -filter_complex "$trim$concat" -map "[out]" "${output:-$base.mp3}"
 
 [ $? -eq 0 ] && echo "File '${output:-$base.mp3}' was created successfully."
