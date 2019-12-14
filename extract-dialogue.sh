46c423bcbb6ba1d4c7f5f599a98f8f85ac6847bd
brennier <bennett@brennier.com>
2019-12-14 10:54:12 -0500

    Remove intermediate files and clean up code

--- extract-dialogue.sh
+++ extract-dialogue.sh
@@ -7,10 +7,9 @@ Options:
     -s    Either specify the subtitle track number to use or specify an external subtitle file
     -o    Specify the output filename
     -p    Specify padding (in milliseconds) around subtitle timestamps
-    -k    Keep intermediate files stored under /tmp; useful for debugging purposes
     -h    Display this usage message
 
-Only the -i option is required. If not specified, the default behavior is to use the first audio track and the first subtitle track. The default output name is "output.mp3". Similar to ffmpeg, the extension of the output name determines the format of the output.\n' "$(basename $0)"
+Only the -i option is required. If not specified, the default behavior is to use the first audio track and the first subtitle track. The default output name is simply the name of the input file with the extension changed to .mp3. The default padding is 100 milliseconds. Similar to ffmpeg, the extension of the output name determines the format of the output.\n' "$(basename $0)"
     exit 0
 }
 
@@ -19,27 +18,23 @@ error() {
     exit 1
 }
 
-extract_timestamps() {
-    # Parses the subtitles file to the format BEGIN:END
-    # BEGIN and END are in milliseconds.
-    # Output is sorted by BEGIN time
+cat_subs() {
     subs=$1
-    padding=$2
     if [ -f "$subs" ]; then
-        ffmpeg -y -loglevel fatal -i "$subs" -f ass "$temp"
+        ffmpeg -y -loglevel fatal -i "$subs" -f ass -
     else
         subs_id=$(ffprobe "$file" 2>&1 | grep "Stream .*Subtitle" \
                   | sed -n "${subs:-1}p" | grep -o '[0-9]:[0-9]')
         [ -z "$subs_id" ] && error "No text-based subtitles found in '$file'."
-        ffmpeg -y -loglevel fatal -i "$file" -map $subs_id -f ass "$temp"
-        [ -f "$temp" ] || error "Extraction failed."
+        ffmpeg -loglevel fatal -i "$file" -map $subs_id -f ass -
     fi
+}
 
-    # Extract the timestamps of dialogue without extra styling.
-    # This will exclude signs and most OPs/EDs.
-    timestamps=$(grep "^Dialogue:" "$temp" | grep -v ",{" \
+extract_timestamps() {
+    # Extracts timestamps into the format BEGIN:END (in milliseconds)
+    padding=$1
+    timestamps=$(grep "^Dialogue:" - | grep -v ",{" \
                  | cut -f "2,3" -d "," | sort)
-
     [ -z "$timestamps" ] && error "Subtitles file was found, but parsing failed."
 
     # Convert timestamps to milliseconds and pad the timestamps
@@ -49,18 +44,14 @@ extract_timestamps() {
 }
 
 merge_timestamps() {
-    # Takes a list of timestamp intervals from standard input and merges
-    # overlapping intervals
+    # Merges overlapping intervals from standard input
     cur_begin=
     cur_end=
-
-    for timestamp in $(cat /dev/stdin); do
-        begin=$(echo "$timestamp" | cut -f1 -d:)
-        end=$(  echo "$timestamp" | cut -f2 -d:)
-        [ -z "$cur_begin" ] && cur_begin=$begin
-        [ -z "$cur_end" ]   && cur_end=$end
-
-        if [ "$cur_end" -lt "$begin" ]; then
+    while IFS=: read -r begin end; do
+        if [ -z "$cur_begin" ]; then
+            cur_begin=$begin
+            cur_end=$end
+        elif [ "$cur_end" -lt "$begin" ]; then
             echo "$cur_begin:$cur_end"
             cur_begin=$begin
             cur_end=$end
@@ -71,8 +62,6 @@ merge_timestamps() {
     echo "$cur_begin:$cur_end"
 }
 
-[ -z "$1" ] && usage
-
 while [ -n "$1" ]; do
     case "$1" in
         "-i") shift; file=$1    ;;
@@ -80,36 +69,28 @@ while [ -n "$1" ]; do
         "-s") shift; subs=$1    ;;
         "-o") shift; output=$1  ;;
         "-p") shift; padding=$1 ;;
-        "-k") keep=1            ;;
         "-h") usage             ;;
-        *) error "There was an error parsing arguments. Make sure to use the -i option." ;;
+        *) error "Failed to parse arguments. Use -h for help." ;;
     esac
     shift
 done
 
-temp=$(mktemp)
-[ -z "$keep" ] && trap 'rm -f $temp' EXIT
-
-timestamps=$(extract_timestamps "$subs" "${padding:-100}" | merge_timestamps \
+timestamps=$(cat_subs "$subs" | extract_timestamps "${padding:-100}" | merge_timestamps \
     | awk -F: '{ printf "%.3f:%.3f\n", ( $1 / 1000 ), ( $2 / 1000 ) }')
 audio_id=$(ffprobe "$file" 2>&1 | grep "Stream .*Audio" \
            | sed -n "${audio:-1}p" | grep -o '[0-9]:[0-9]')
-ext=$(echo "${output:-output.mp3}" | sed 's/.*\.//')
 
 # Setup the filter complex command for trimming and then concatenating
-num=1
+num=0
 trim=
 concat=
 for timestamp in $timestamps; do
+    num=$(( $num + 1 ))
     trim="$trim[$audio_id]atrim=$timestamp[a$num];"
     concat="$concat[a$num]"
-    num=$(( $num + 1 ))
 done
-num=$(( $num - 1 ))
 concat="${concat}concat=n=$num:v=0:a=1[out]"
 
-base=$(basename "$file" | sed 's/\.[^.]*$//')
-echo "Concatenating audio files..."
-ffmpeg -i "$file" -filter_complex "$trim$concat" -map "[out]" "${output:-$base.mp3}"
-
-[ $? -eq 0 ] && echo "File '${output:-$base.mp3}' was created successfully."
+output=${output:-"$(basename "$file" | sed 's/\.[^.]*$//').mp3"}
+ffmpeg -i "$file" -filter_complex "$trim$concat" -map "[out]" "$output"
+[ $? -eq 0 ] && echo "File '$output' was created successfully."
